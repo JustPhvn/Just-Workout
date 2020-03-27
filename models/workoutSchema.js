ea04ddcac50f465f4d1c99f081af420089971a78
JustPhvn <justicequyphan@gmail.com>
2020-03-27 11:11:25 -0700

    Changed file structure and edited workout schema

---
+++ models/workoutSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const workoutSchema = mongoose.Schema({
+  date: {
+    type: Date,
+    default: Date.now(),
+    required: true
+  },
+  duration: {
+    type: Number,
+    require: true
+  },
+  name: {
+    type: Number,
+    require: true
+  },
+  weight: {
+    type: Number
+  },
+  sets: {
+    type: Number
+  },
+  reps: {
+    type: Number
+  },
+  distance: {
+    type: Number
+  }
+});
+
+const workout = mongoose.model("workout", workoutSchema);
+
+module.exports = Workout;
