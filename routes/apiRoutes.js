3ad91de49cd8d81f3f3072721c1bdb23a6e78f04
JustPhvn <justicequyphan@gmail.com>
2020-04-10 11:55:46 -0700

    Fixed links between files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -20,13 +20,27 @@ router.post("/api/workouts", (req, res) => {
       res.json(dbWorkout);
     })
     .catch(err => {
+      console.log(err);
       res.status(400).json(err);
     });
 });
 
 router.put("/api/workouts/:id", (req, res) => {
   workout
-    .findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .findByIdAndUpdate(req.params.id, {
+      $push: { exercises: req.body }
+    })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", function(req, res) {
+  workout
+    .find({})
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
